c0299ddfbd31b1f92de8f2a85db266071437a548
mahesh13614 <113846307+mahesh13614@users.noreply.github.com>
2025-10-16 06:45:35 +0000

    retitling

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Mahesh Sampath';
+    : 'Life Examined';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Nothing in particular';
+    : 'Essays on whatever seems worth thinking about';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
