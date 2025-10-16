1e51e72a3290f9850a334d5c180c9a3f9f4e097a
mahesh13614 <113846307+mahesh13614@users.noreply.github.com>
2025-10-16 06:11:03 +0000

    title

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Mahesh Sampath';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Nothing in particular';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
